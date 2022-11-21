910bf0ee90cfaabb401e9a74c6304887aee0dac3
Oscar Ibarra <oscaribarra@Oscars-MacBook-Air.local>
2022-11-21 10:55:36 -0800

    geofire

---
+++ lib/assistants/assistant_methods.dart
@@ -0,0 +1,85 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+
+import 'package:uber_copy/assistants/request_assistant.dart';
+import 'package:uber_copy/global/map_key.dart';
+import 'package:uber_copy/infoHandler/app_info.dart';
+import 'package:uber_copy/models/direction_details_info.dart';
+
+import '../global/global.dart';
+
+import '../models/directions.dart';
+import '../models/user_model.dart';
+
+class AssistantMethods {
+  static Future<String> searchAddressForGeographicCoordinates(
+      Position position, context) async {
+    String apiUrl =
+        "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$mapKey";
+    String humanReadableAddress = "";
+
+    var requestResponse = await RequestAssistant.receiveRequest(apiUrl);
+
+    if (requestResponse != "Failed") {
+      humanReadableAddress = requestResponse["results"][0]["formatted_address"];
+
+      Directions userPickUpAddress = Directions();
+      userPickUpAddress.locationLatitude = position.latitude;
+      userPickUpAddress.locationLongitude = position.longitude;
+      userPickUpAddress.locationName = humanReadableAddress;
+
+      Provider.of<AppInfo>(context, listen: false)
+          .updatePickUpLocationAddress(userPickUpAddress);
+    }
+
+    return humanReadableAddress;
+  }
+
+  static void readCurrentOnlineUserInfo() {
+    currentFirebaseUser = fAuth.currentUser;
+    DatabaseReference userRef = FirebaseDatabase.instance
+        .ref()
+        .child("users")
+        .child(currentFirebaseUser!.uid);
+
+    userRef.once().then((snap) {
+      if (snap.snapshot.value != null) {
+        userModelCurrentInfo = UserModel.fromSnapshot(snap.snapshot);
+      }
+    });
+  }
+
+  static Future<DirectionDetailsInfo?>
+      obtainOriginToDestinationDirectionDetails(
+          LatLng originPosition, LatLng destinationPosition) async {
+    String urlOriginToDestinationDirectionDetails =
+        "https://maps.googleapis.com/maps/api/directions/json?origin=${originPosition.latitude},${originPosition.longitude}&destination=${destinationPosition.latitude},${destinationPosition.longitude}&key=$mapKey";
+
+    var responseDirectionApi = await RequestAssistant.receiveRequest(
+        urlOriginToDestinationDirectionDetails);
+
+    if (responseDirectionApi == "Failed") {
+      return null;
+    }
+
+    DirectionDetailsInfo directionDetailsInfo = DirectionDetailsInfo();
+
+    directionDetailsInfo.e_points =
+        responseDirectionApi["routes"][0]["overview_polyline"]["points"];
+
+    directionDetailsInfo.distance_text =
+        responseDirectionApi["routes"][0]["legs"][0]["distance"]["text"];
+    directionDetailsInfo.distance_value =
+        responseDirectionApi["routes"][0]["legs"][0]["distance"]["value"];
+
+    directionDetailsInfo.duration_text =
+        responseDirectionApi["routes"][0]["legs"][0]["duration"]["text"];
+    directionDetailsInfo.duration_value =
+        responseDirectionApi["routes"][0]["legs"][0]["duration"]["value"];
+
+    return directionDetailsInfo;
+  }
+}
--- lib/tabPages/home_tab.dart
+++ lib/tabPages/home_tab.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
 
+import 'package:flutter_geofire/flutter_geofire.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uber_copy/global/global.dart';
+import 'package:uber_copy/infoHandler/app_info.dart';
+import '../assistants/assistant_methods.dart';
 
 class HomeTabPage extends StatefulWidget {
   const HomeTabPage({super.key});
@@ -19,6 +25,14 @@ class _HomeTabPageState extends State<HomeTabPage> {
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+  Position? driverCurrentPosition;
+  var geoLocator = Geolocator();
+  LocationPermission? _locationPermission;
+
+  String statusText = "Now Offline";
+  Color buttonColor = Colors.grey;
+  bool isDriverActive = false;
 
   blackThemeGoogleMap() {
     newGoogleMapController!.setMapStyle('''
@@ -186,6 +200,36 @@ class _HomeTabPageState extends State<HomeTabPage> {
                 ''');
   }
 
+  checkIfLocationpermissionAllowed() async {
+    _locationPermission = await Geolocator.requestPermission();
+    if (_locationPermission == LocationPermission.denied) {
+      _locationPermission = await Geolocator.requestPermission();
+    }
+  }
+
+  locateDriverPosition() async {
+    Position cPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    driverCurrentPosition = cPosition;
+
+    LatLng latLngPosition = LatLng(
+        driverCurrentPosition!.latitude, driverCurrentPosition!.longitude);
+    CameraPosition cameraPosition =
+        CameraPosition(target: latLngPosition, zoom: 14);
+    // newGoogleMapController!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    // String humanReadableAddress = await AssistantMethods.searchAddressForGeographicCoordinates(userCurrentPosition!, context);
+    // print("this is your address = " + humanReadableAddress);
+    await AssistantMethods.searchAddressForGeographicCoordinates(
+        driverCurrentPosition!, context);
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   checkIfLocationpermissionAllowed();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -200,9 +244,75 @@ class _HomeTabPageState extends State<HomeTabPage> {
 
             //black theme
             blackThemeGoogleMap();
+
+            locateDriverPosition();
           },
-        )
+        ),
+
+        //ui for online offline driver
+        statusText != "Now Online"
+            ? Container(
+                height: MediaQuery.of(context).size.height,
+                width: double.infinity,
+                color: Colors.black87,
+              )
+            : Container(),
+
+        //button for online offline driver
+        Positioned(
+            top: statusText != "Now Online"
+                ? MediaQuery.of(context).size.height * 0.45
+                : 25,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      // driverIsOnlineNow();
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: buttonColor,
+                        padding: const EdgeInsets.symmetric(horizontal: 18),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(26),
+                        )),
+                    child: statusText != "Now Online"
+                        ? Text(statusText,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ))
+                        : const Icon(
+                            Icons.phonelink_ring,
+                            color: Colors.white,
+                            size: 26,
+                          ))
+              ],
+            ))
       ],
     );
   }
+
+  // driverIsOnlineNow() async {
+  //   Position pos = await Geolocator.getCurrentPosition(
+  //     desiredAccuracy: LocationAccuracy.high,
+  //   );
+  //   driverCurrentPosition = pos;
+
+  //   Geofire.initialize("activeDrivers");
+  //   Geofire.setLocation(currentFirebaseUser!.uid,
+  //       driverCurrentPosition!.latitude, driverCurrentPosition!.latitude);
+
+  //   DatabaseReference ref = FirebaseDatabase.instance
+  //       .ref()
+  //       .child("drivers")
+  //       .child(currentFirebaseUser!.uid)
+  //       .child("newRideStatus");
+
+  //   ref.set("idle");
+  //   ref.onValue.listen((event) {});
+  // }
 }
